32c171092db6ad24902d33240af6e4c595c1e303
John Sawyer <sawyer@sawyermerchant.com>
2016-11-09 19:39:58 -0500

    possible moves array

--- lib/move_tree.rb
+++ lib/move_tree.rb
@@ -1,6 +1,27 @@
 Move = Struct.new(:x, :y, :depth, :children, :parent)
 
 class MoveTree
-  
+  def initialize(coords, max_depth)
+    @coords = coords
+    @width = 5
+  end
+
+  def build_tree
+    move_coords = Array.new(8) {@coords}
+    @coord
+  end
 end
 
+
+
+[-2, -1, 1, 2]. each
+  [-1, -2, 2, 1]
+
+[-2, -1]
+[-2, 1]
+[-1, -2]
+[-1, 2]
+[1, -1]
+[1, 2]
+[2, -1]
+[2, 1]
--- spec/move_tree_spec.rb
+++ spec/move_tree_spec.rb
@@ -1,13 +1,16 @@
 require 'move_tree'
 
-describe MoveTree do 
-  
-  describe '#initialize' do 
+describe MoveTree do
 
-    it 'takes start coordinates & max depth as arguments' do 
-      
+  describe '#build_tree' do
+
+  let(:test_tree) {MoveTree.new([0,0],1)}
+
+    it 'returns array of all possible moves' do
+      moves = test_tree.build_tree
+      expect(moves[0].x).to eq(2)
+      expect(moves[0].y).to eq(1)
     end
-
   end
 
 end
